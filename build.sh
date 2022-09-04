4a92d3259593aad0ba4b539ce131822f78dd8bbd
haeramkeem <ewqdsacxz2345@gmail.com>
2022-09-04 16:10:53 +0900

    feat: build work JSON

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Envs
+SOURCE_DIR=contents
+OUTPUT_DIR=dist
+OUTPUT=$OUTPUT_DIR/works
+
+# Generate file
+mkdir -p $OUTPUT_DIR
+echo '[' > $OUTPUT
+echo "Build output: $OUTPUT"
+
+# Convert YAML to JSON
+for man in $(ls $SOURCE_DIR/*.yaml); do
+    echo "Converting $man"
+    yq -o json eval $man | jq -c . >> $OUTPUT
+done
+
+# Finalize
+echo ']' >> $OUTPUT
+
+# Fix JSON syntax
+cat $OUTPUT | tr -d '\n' | sed 's/}{/},{/g' | tee $OUTPUT
+echo; echo "Done"
