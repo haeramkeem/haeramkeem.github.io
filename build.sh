73c9a45949f42fe44064d1d4254cd770fee7a2a3
haeramkeem <ewqdsacxz2345@gmail.com>
2022-09-04 23:20:14 +0900

    feat: copy non-webpacked data with build.sh

--- build.sh
+++ build.sh
@@ -2,25 +2,37 @@
 
 set -e
 
-# Envs
-SOURCE_DIR=contents
-OUTPUT_DIR=dist/api
-OUTPUT=$OUTPUT_DIR/works
+build_json() {
+    # Envs
+    local SOURCE_DIR=$1
+    local OUTPUT_DIR=dist/$2
+    local OUTPUT=$OUTPUT_DIR/works
 
-# Generate file
-mkdir -p $OUTPUT_DIR
-echo '[' > $OUTPUT
-echo "Build output: $OUTPUT"
+    # Generate file
+    mkdir -p $OUTPUT_DIR
+    echo '[' > $OUTPUT
+    echo "Build output: $OUTPUT"
 
-# Convert YAML to JSON
-for man in $(ls $SOURCE_DIR/*.yaml); do
-    echo "Converting $man"
-    yq -o json eval $man | jq -c . >> $OUTPUT
-done
+    # Convert YAML to JSON
+    for man in $(ls $SOURCE_DIR/*.yaml); do
+        echo "Converting $man"
+        yq -o json eval $man | jq -c . >> $OUTPUT
+    done
 
-# Finalize
-echo ']' >> $OUTPUT
+    # Finalize
+    echo ']' >> $OUTPUT
 
-# Fix JSON syntax
-cat $OUTPUT | tr -d '\n' | sed 's/}{/},{/g' | tee $OUTPUT
-echo; echo "Done"
+    # Fix JSON syntax
+    cat $OUTPUT | tr -d '\n' | sed 's/}{/},{/g' | tee $OUTPUT
+    echo; echo "Done"
+}
+
+copy_public_data() {
+    local SOURCE_DIR=$1
+    local OUTPUT_DIR=dist
+
+    cp $SOURCE_DIR/* $OUTPUT_DIR/.
+}
+
+build_json contents api
+copy_public_data public
